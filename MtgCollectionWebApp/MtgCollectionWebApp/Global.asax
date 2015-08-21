3441df6e4010c33b04bdb945c73a00608e9c9917
Daniel.Baranowski <Daniel.Baranowski@bjss.co.uk>
2015-08-21 14:06:57 +0100

    Initial Commit template MVC app

---
+++ MtgCollectionWebApp/MtgCollectionWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MtgCollectionWebApp.MvcApplication" Language="C#" %>
---
+++ MtgCollectionWebApp/MtgCollectionWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MtgCollectionWebApp.Startup))]
+namespace MtgCollectionWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
