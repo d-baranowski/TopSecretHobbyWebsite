e92f9a047751b6aa770aed9e338a1c8b89f7ce74
Daniel.Baranowski <Daniel.Baranowski@bjss.co.uk>
2015-08-28 09:39:42 +0100

    Managed to make the quantity put request work

--- MtgCollectionWebApp/MtgCollectionWebApp/Controllers/QuantityController.cs
+++ MtgCollectionWebApp/MtgCollectionWebApp/Controllers/QuantityController.cs
@@ -32,29 +32,39 @@ namespace MtgCollectionWebApp.Models
         // PUT: api/Quantity/5
         public void Put(int id, [FromBody]string value)
         {
-            var getEntriesUrl = "api/Entries";
+            var getEntriesUrl = "http://localhost:59756/api/Entries";
             HttpWebRequest getRequest = (HttpWebRequest)WebRequest.Create(getEntriesUrl);
             getRequest.Method = WebRequestMethods.Http.Get;
             getRequest.Accept = "application/json";
             var getResponse = getRequest.GetResponse();
             var sr = new StreamReader(getResponse.GetResponseStream());
-
             var text = sr.ReadToEnd();
-            JObject entries = JObject.Parse(text);
+            JArray entries = JArray.Parse(text);
+
             for (int i = 0; i < entries.Count; i++)
             {
-                var entryId = Int32.Parse((string)entries[i]["collectionEntry"]);
+                var entryId = Int32.Parse((string)entries[i]["CollectionEntryId"]);
                 if (entryId == id) //Entry for given card already exists 
                 {
-                    var updateEntryUrl = "api/Entries/"+entryId;
-                    var client = new WebClient();
-                    client.BaseAddress = updateEntryUrl;
-                    
-                    var data = new CollectionEntry();
-                    data.CollectionEntryId = id;
-                    data.CollectionEntryCardId = id;
-                    data.CollectionId = Int32.Parse((string)entries[i]["CollectionId"]);
-                    data.Quantity = Int32.Parse((string)entries[i]["Quantity"]) + 1;
+                    var updateEntryUrl = "http://localhost:59756/api/Entries/" + entryId;
+
+                    var eId = id;
+                    var cecId = id;
+                    var cid = (string)entries[i]["CollectionId"];
+                    var q = Int32.Parse((string)entries[i]["Quantity"]) + 1;
+                    string dataString = "{ \"CollectionEntryId\":"+ eId + ",\"Quantity\":"+ q + ",\"CollectionEntryCardId\":"+ cecId + ",\"CollectionId\":"+ cid + "}";
+
+                    HttpWebRequest putRequest = (HttpWebRequest)WebRequest.Create(updateEntryUrl);
+                    putRequest.Method = WebRequestMethods.Http.Put;
+                    putRequest.ContentType = "application/json";
+                    putRequest.Accept = "application/json";
+
+                    var cli = new WebClient();
+                    cli.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    string response = cli.UploadString(updateEntryUrl,"PUT",dataString);
+                    //var sw = new StreamWriter(putRequest.GetRequestStream());
+                    //sw.Write(dataString);
+                    //putRequest.GetResponse();
                 } 
             }
 
