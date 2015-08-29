0ee87cfd67baf3787cc43b3545cfc7c0562c6f39
Daniel_Baranowski <d.m.baranowski@newcastle.ac.uk>
2015-08-29 10:31:49 +0100

    Finished refactoring. Known issue: Don't know how to put scripts in separate files. Procede to mplementing anglar js

---
+++ MtgCollectionWebApp/MtgCollectionWebApp/Scripts/Cards/changeQuantity.js
@@ -0,0 +1,12 @@
+﻿//AJAX function
+function startAjax(i, val) {
+    $.ajax({
+        url: 'http://localhost:59756/api/Quantity/' + i + "?value=" + val,
+        type: "PUT",
+    }).done(function (data) {
+    }).fail(function (err) {
+        alert('fail')
+    }).success(function (result) {
+    });
+
+}
