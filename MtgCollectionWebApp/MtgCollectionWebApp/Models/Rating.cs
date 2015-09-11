7d523251ed530366dee0774de2c9300811e0f238
Daniel.Baranowski <Daniel.Baranowski@bjss.co.uk>
2015-09-11 20:27:50 +0100

    Trying to fix the broken repo

--- MtgCollectionWebApp/MtgCollectionWebApp/Models/Rating.cs
+++ MtgCollectionWebApp/MtgCollectionWebApp/Models/Rating.cs
@@ -6,7 +6,7 @@ namespace MtgCollectionWebApp.Models
     {
         [Key]
         public int RatingId { get; set; }
-        public int UserId { get; set; } //Who's rating is it
+        public int UserId { get; set; } //Who's rating is it test push goddammit
         public int RatingValue { get; set; } //Rating 0 to 5
         public virtual string RatingCardName { get; set; } //Name of the rated card
     }
